2a74d85c2121d68979f9118bab8310149348d455
MelinaJB <melina.jb@hotmail.com>
2024-11-10 20:44:30 -0300

    cambios pdf

--- oc/models.py
+++ oc/models.py
@@ -21,12 +21,11 @@ class OrdenCompra(models.Model):
 
     numero = models.CharField(max_length=20, default='4500', unique=True)
     fecha = models.DateField(blank=True, null=True)
-    nrocompulsa = models.CharField(max_length=20,default='4500', blank=True, null=True)
+    nrocompulsa = models.CharField(max_length=20,default='0000-0000', blank=True, null=True)
     afiliado = models.CharField(max_length=200, blank=True, null=True)
     domicilioafiliado = models.CharField(max_length=300, blank=True, null=True)
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     importe_total = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True)
-
     estado = models.CharField(max_length=20, choices=ESTADO_CHOICES, default='pendiente')
     entrega_unica = models.BooleanField(default=True)
 
@@ -45,8 +44,16 @@ class Producto(models.Model):
     
 
 #SUBIR PDF ////////////////
+# class PDFDocument(models.Model):
+    # file = models.FileField(upload_to='pdfs/')
+    # extracted_data = models.TextField(blank=True, null=True)  # Puedes ajustar el tipo de campo según la información
+    # uploaded_at = models.DateTimeField(auto_now_add=True)
+
 class PDFDocument(models.Model):
+    # orden_compra = models.ForeignKey(OrdenCompra, on_delete=models.CASCADE, related_name='pdfs', default=1)  # Asumiendo que 1 es un ID válido de OrdenCompra
     file = models.FileField(upload_to='pdfs/')
-    extracted_data = models.TextField(blank=True, null=True)  # Puedes ajustar el tipo de campo según la información
+    extracted_data = models.TextField(blank=True, null=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'{self.orden_compra.numero} - PDF'
--- oc/views.py
+++ oc/views.py
@@ -42,6 +42,102 @@ class listadoOC(TemplateView):
         })
 
 
+# class nuevaOC(TemplateView):
+#     template_name = 'nueva_oc.html' 
+    
+#     def get(self, request):
+#         pdf_form = PDFUploadForm()
+#         oc_form = nuevaOCForm()
+#         return render(request, self.template_name, {
+#             'pdf_form': pdf_form,
+#             'oc_form': oc_form,
+#             'extraido': None,
+#         })
+
+#     def post(self, request):
+#         if 'upload_pdf' in request.POST:
+#             # Solo procesar el formulario PDF, pero no guardarlo todavía
+#             pdf_form = PDFUploadForm(request.POST, request.FILES)
+#             if pdf_form.is_valid():
+#                 pdf_instance = pdf_form.save(commit=False)  # No guardar todavía, solo crear la instancia)
+#                 datos = extraer_datos(pdf_instance.file.path)
+#                 cliente = Cliente.objects.get(cliente=datos.get('uoc'))
+
+#                 print("Datos extraídos:", datos)
+                
+#                 # Llenar el formulario de la orden de compra con los datos extraídos
+#                 oc_form = nuevaOCForm(initial={
+#                     'numero': datos.get('numero_orden'),
+#                     'fecha': datos.get('fecha_orden'),
+#                     'nrocompulsa': datos.get('numero_compulsa'),
+#                     'afiliado': datos.get('nombre_afiliado'),
+#                     'domicilioafiliado': "Dirección no especificada en el PDF",
+#                     'cliente': cliente.id,
+#                     'estado': 'pendiente',
+#                     'importe_total': datos.get('importe_total')
+#                 })
+
+#                 productos = datos.get('detalle_orden', [])
+                
+#                 print("Productos extraídos:", productos)
+
+#                 return render(request, self.template_name, {
+#                     'pdf_form': pdf_form,
+#                     'oc_form': oc_form,
+#                     'productos': productos,
+#                 })
+#             else:
+#                 return render(request, self.template_name, {
+#                     'pdf_form': pdf_form,
+#                     'oc_form': nuevaOCForm()
+#                 })
+
+#         else:
+#             oc_form = nuevaOCForm(request.POST)
+#             if oc_form.is_valid():
+#                 orden_compra = oc_form.save()
+#                 print("Orden de compra guardada:", orden_compra)
+
+#                 productos_data = request.POST.getlist('productos')
+
+#                 cantidad_list = request.POST.getlist('cantidad')
+#                 descripcion_list = request.POST.getlist('descripcion')
+#                 precio_unitario_list = request.POST.getlist('precio_unitario')
+
+                
+#                 # Guardar los productos asociados a la orden de compra
+#                 for i in range(len(cantidad_list)):
+#                     Producto.objects.create(
+#                         orden_compra=orden_compra,
+#                         cantidad=cantidad_list[i],
+#                         descripcion=descripcion_list[i],
+#                         precio_unitario=precio_unitario_list[i]
+#                         )
+#                     print("Producto guardado:", {
+#                         'cantidad': cantidad_list[i],
+#                         'descripcion': descripcion_list[i],
+#                         'precio_unitario': precio_unitario_list[i]
+#                         })
+                    
+#                 # Guardar el PDF una vez que la orden de compra se guardó
+#                 if 'pdf' in request.FILES:
+#                     pdf_file = request.FILES['pdf']
+#                     pdf_document = PDFDocument.objects.create(
+#                         orden_compra=orden_compra,
+#                         file=pdf_file
+#                     )
+#                 print("PDF asociado a la orden de compra:", pdf_document)
+                
+#                 return redirect('oc:listado_oc')
+#             else:
+#                 print("Formulario de OC no válido:", oc_form.errors)
+        
+#         return render(request, self.template_name, {
+#             'pdf_form': PDFUploadForm(),
+#             'oc_form': oc_form
+#         })    
+
+
 class nuevaOC(TemplateView):
     template_name = 'nueva_oc.html' 
     
@@ -97,7 +193,6 @@ class nuevaOC(TemplateView):
                 print("Orden de compra guardada:", orden_compra)
 
                 productos_data = request.POST.getlist('productos')
-
                 cantidad_list = request.POST.getlist('cantidad')
                 descripcion_list = request.POST.getlist('descripcion')
                 precio_unitario_list = request.POST.getlist('precio_unitario')
@@ -125,8 +220,6 @@ class nuevaOC(TemplateView):
             'pdf_form': PDFUploadForm(),
             'oc_form': oc_form
         })    
-
-
 #MODIFICAR OC
 class modificarOC(TemplateView):
     def get(self, request, id):
