7372539b529a2eecf50e7c4bb7e4e18bf24283ad
MelinaJB <93693282+MelinaJB@users.noreply.github.com>
2024-11-11 17:18:21 -0300

    Update wsgi.py

--- tpfinal/wsgi.py
+++ tpfinal/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.core.management import execute_from_command_line
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tpfinal.settings')
 
 application = get_wsgi_application()
+
+# Esto asegura que el servidor use el puerto que Render asigna
+port = os.getenv('PORT', '8000')  # Render asigna un puerto dinámico, se usa el 8000 por defecto
